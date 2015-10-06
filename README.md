86ea23c87fcba4ad9466c44dcae1405b55a38507
System Administrator <aerrity@gmail.com>
2015-10-06 17:41:58 +0100

    Initial library commit

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # Inputs
-Processing library to provide keyboard input via dialog boxes.
+## About
+A simple library for the Processing programming language that facilitates keyboard input via dialog boxes.
+## Installation
+Download this repository and copy the entire Inputs folder into the libraries folder in your Processing sketchbook folder. Your sketchbook folder is typically in  '/users/username/Documents/Processing/libraries' (you can check the exact folder location in the Processing preferences). 
+
+When your done the folder structure should look like
+- Documents
+ - Processing
+  - libraries
+   - Inputs
+
+When this is completed, restart Processing and the library is installed. 
+
+## Use
+It can be imported into any sketch using:
+```java
+import iadt.creative.Inputs;
+```
+
+You can access an example of the library in use by clicking 'File->Examples...->Contributing Libraries->Inputs->Inputs Example' in the Processing IDE.
---
+++ src/Inputs.java
@@ -0,0 +1,92 @@
+/*
+ * Keyboard input library
+ * v0.2
+ * Andrew Errity 1/10/15
+ */
+import javax.swing.JOptionPane;
+import javax.swing.JTextField;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import java.awt.BorderLayout;
+
+public static class Inputs{
+
+  public static String readString(String textLabel){
+    JTextField field  = new JTextField(20);
+    JLabel label = new JLabel(textLabel);
+    JPanel p = new JPanel(new BorderLayout(5, 2));
+    p.add(label, BorderLayout.WEST);
+    p.add(field);
+    JOptionPane.showMessageDialog(null, p, "Input required", JOptionPane.PLAIN_MESSAGE, null);
+    String in = field.getText();
+    return in; 
+  }
+  
+   public static byte readByte(String label){
+    String in = readString(label);
+    if (in == null) {
+      //exit();
+      return 0;
+    }
+    else
+      return Byte.parseByte(in);
+  }
+  
+   public static short readShort(String label){
+    String in = readString(label);
+    if (in == null) {
+      //exit();
+      return 0;
+    }
+    else
+      return Short.parseShort(in);
+  }
+  
+   public static int readInt(String label){
+    String in = readString(label);
+    if (in == null) {
+      //exit();
+      return 0;
+    }
+    else
+      return Integer.parseInt(in);
+  }
+  
+   public static long readLong(String label){
+    String in = readString(label);
+    if (in == null) {
+      //exit();
+      return 0;
+    }
+    else
+      return Long.parseLong(in);
+  }
+  
+   public static float readFloat(String label){
+    String in = readString(label);
+    if (in == null) {
+      //exit();
+      return 0;
+    }
+    else
+      return Float.parseFloat(in);
+  }
+  
+   public static double readDouble(String label){
+    String in = readString(label);
+    if (in == null) {
+      //exit();
+      return 0;
+    }
+    else
+      return Double.parseDouble(in);
+  }
+  
+   public static boolean readBoolean(String label){
+    int in = JOptionPane.showConfirmDialog(null,"choose one", "choose one", JOptionPane.YES_NO_OPTION);
+    if (in == JOptionPane.YES_OPTION) 
+      return true;
+    else 
+      return false;
+  }
+}
